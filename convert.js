f23a4e13b29f81621f8328ff3bca4980b59b6b11
Dave Lee <camperdave614@gmail.com>
2014-12-18 13:16:30 -0600

    fix a bunch of sloppy errors, test on internet

--- convert.js
+++ convert.js
@@ -2,20 +2,38 @@ const MIME_TYPE = 'text/csv';
 const SKIP_SETS = ['DPA', 'EVENT', 'VAN', ' '];
 
 function processCSV(results, file) {
-	var cards = [];
+	var cards = {};
+	console.log(results);
+	console.log(file);
+
+
 	// First, parse the CSV list, and build an array of objects that combine foil and non-foil cards.
 	// Fuck performance, we're going to burn space and memory like it's 2014 just in case MTGO does something stupid (seems likely someday)
-	for (var row in results.data) 
+	for (var i = 0; i < results.data.length; i++) 
 	{
+		var row = results.data[i];
+		if (typeof row === "undefined") continue;
+
 		var name = row["Card Name"];
+		if (typeof name === "undefined" || name === "") continue;				// Skip blank rows.
+
 		var setCode = row["Set"];
 		var card_id = name+"~"+setCode;
-		var card = cards[card_id] || { name: name, setCode: setCode, nonFoil: 0, foil: 0 };
+		var card = cards[card_id];
+		
+		if (typeof card === "undefined")  {
+			card = { 
+				name: name, 
+				setCode: setCode, 
+				nonFoil: 0, 
+				foil: 0 
+			};
+		}
 		if (row["Premium"] == "Yes") {
-			card.foil += row["Quantity"];
+			card.foil += parseInt(row["Quantity"]);
 		}
 		else {
-			card.nonFoil += row["Quantity"];
+			card.nonFoil += parseInt(row["Quantity"]);
 		}
 		cards[card_id] = card;
 	}
@@ -23,10 +41,11 @@ function processCSV(results, file) {
 
 	// Then, create a JSON array of objects that store the data in a convert_coll compatible format
 	var outputJSON = [];
-	for (var card in cards) {
+	for (var card_id in cards) {
+		var card = cards[card_id];
 		outputJSON.push({
 			"Card Name"    : card.name,
-			"Cardset"      : set_trans[card.set],				//see table.js for the giant map of set codes.
+			"Cardset"      : set_trans[card.setCode],				//see table.js for the giant map of set codes. Surround the code in '' so we can deal with 2ED style codes.
 			"# Owned"      : card.nonFoil,
 			"# Foil Owned" : card.foil
 		});
@@ -34,9 +53,10 @@ function processCSV(results, file) {
 
 	// Next, we convert that JSON object back to CSV
 	var csv = Papa.unparse(outputJSON);
+	console.log(csv);
 
 	// Finally, we need to create a blob and create a download link for the user.
-	var blob = new Blob([csv_out], {type: MIME_TYPE});
+	var blob = new Blob([csv], {type: MIME_TYPE});
 
 	var a_href = window.URL.createObjectURL(blob);
 	var a_download = 'decked_' + file.name;
@@ -58,20 +78,10 @@ $(document).ready(function () {
 			config: {
 				header: true,
 				complete: processCSV,
-				worker: true,
+				//worker: true,
 			},
 			before: function(file, inputElem) {
-				if(file.size === 0) {
-					$("#msgOutpud").append("<li>Skipping " + file.name + " as it is blank!</li>");
-					console.log(file);
-					return {
-						action: "abort",
-						reason: "blank / no file"
-					}
-				}
-				else {
-					$("#msgOutput").append("<li>Started processing " + file.name + "!</li>");
-				}
+				$("#msgOutput").append("<li>Started processing " + file.name + "!</li>");
 			},
 			error: function(err, file, inputElem, reason) {
 				$('#msgOutput').append("<li>Something error happen. Check Console.Log for now!</li>");
